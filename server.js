fad32ebc9884fd4e0b4c9fea3f74fa853c007db9
enoch-bolarinwa <enoch-bolarinwa@>
2025-11-01 10:36:04 +0100

    server

--- server.js
+++ server.js
@@ -1,31 +1,18 @@
-// Import dependencies
-const express = require('express');
-const expressLayouts = require('express-ejs-layouts');
-const path = require('path');
+import express from 'express';
+import expressLayouts from 'express-ejs-layouts';
 
-// Initialize the app
 const app = express();
 
-// Set EJS as the view engine
 app.set('view engine', 'ejs');
+app.set('views', './views');
+app.set('layout', 'layouts/layout'); 
 
-// Tell Express where the views folder is located
-app.set('views', path.join(__dirname, 'views'));
-
-// Use express-ejs-layouts
 app.use(expressLayouts);
+app.use(express.static('public'));
 
-// Tell Express where to find static files (CSS, images, etc.)
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Index route
-app.get('/', function (req, res) {
-  res.render('index', { title: 'Home' }); 
-
+app.get('/', (req, res) => {
+  res.render('index', { title: 'Home' });
 });
 
-// Start the server
 const PORT = 5500;
-app.listen(PORT, () => {
-  console.log(`✅ Server is running on http://localhost:${PORT}/`);
-});
+app.listen(PORT, () => console.log(`✅ Server running on http://localhost:${PORT}`));
